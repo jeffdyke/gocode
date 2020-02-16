83529e2035ff959100de61e3a79b0a5b53200bb8
Jeff Dyke <jeff.dyke@gmail.com>
2020-02-15 21:17:30 -0500

    take in args, no hardcoded vals

--- main.go
+++ main.go
@@ -3,12 +3,19 @@ package main
 import (
 	"github.com/jeffdyke/jssh"
 	"log"
+	"flag"
+	"os/user"
 )
 
 
 
 func main() {
-	var jump = jssh.PublicKeyConnection{ConnectionInfo:jssh.ConnectionInfo{User: "jeff", Host: "jump.bondlink.com"}}
+	var u, _ = user.Current()
+	var usr = flag.String("user", u.Username, "Defaults to your login name" )
+	var host = flag.String("host", "", "Required must specify host, if using bastion see that help")
+	//var bastion = flag.String("bastion", "", "Required if host")
+	flag.Parse()
+	var jump = jssh.PublicKeyConnection{ConnectionInfo:jssh.ConnectionInfo{User: *usr, Host: *host}}
 	c, e := jump.Connect()
 	if e != nil {
 		log.Panicf("what the fuck is %v", e)
